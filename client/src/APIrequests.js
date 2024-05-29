87cc3e160f49d83d66ce25448d65c9694d4a9c70
thipora <tz0556727951@gmail.com>
2024-05-29 07:57:32 +0300

    start project

---
+++ client/src/APIrequests.js
@@ -0,0 +1,20 @@
+
+class APIrequests {
+    async postRequest(url, body) {
+        try {
+            const response = await fetch('http://localhost:8080' + url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            });
+            return response;
+        } catch(error) {
+            alert(error.message);
+        };
+       
+
+           
+    }
+}
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import CryptoJS from 'crypto-js';
+import {APIrequests} from '../APIrequests.js'
+import { useState, useContext } from 'react'
+import {
+  useNavigate, Link
+} from "react-router-dom";
+import "../css/style.css";
+import { UserContext } from '../UserProvider.jsx';
+
+ function Login() {
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    // const navigate=useNavigate()
+    const { updateUserID } = useContext(UserContext);
+
+    const generatePasswordHash = (password) => {
+      const hashedPassword = CryptoJS.SHA256(password).toString();
+      return hashedPassword;
+    };
+    const hash_password = generatePasswordHash(password);
+
+    function login(){
+      response = APIrequests.postRequest(`/login`, { "email": email, "password_hash": hash_password });
+      if (!response.ok)
+            throw new Error('error, please try egain whith anouther email or password or sign up')
+     
+     const data= response.json();
+            localStorage.setItem("TOKEN", data.accessToken);
+
+        .then(() => getUser())
+    }
+
+
+    function getUser(){
+      const token = localStorage.getItem('TOKEN');
+      fetch(`http://localhost:8080/users?email=${email}`,{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        if(!response.ok)
+          throw new Error('ERROR: ' + response.status + ' ' + response.message)
+        return response.json();
+      })
+      .then(data => {
+        const currentUser = data[0];
+        localStorage.setItem("currentUser", JSON.stringify(currentUser));
+        updateUserID();
+        navigate(`/user/${currentUser.id}/home`);
+      }).catch(error => {
+          alert(error.message);
+        });
+    }
+F
+  return (
+    <>
+      <input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
+      <input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
+      <button onClick={login}>Login</button>
+      <Link to="/register">New User?</Link>
+    </>
+  )
+}
+
+export default Login
