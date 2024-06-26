b1993faa02984f0c978d2bbe8de9935fdd4ee5d5
shira-l <michalla37@gmail.com>
2024-06-27 02:32:52 +0300

    need to check register

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,7 +22,24 @@ export default function Login() {
       password: '',
     }
   })
-
+  const emailValidate = {
+    required: "אנא הכנס את המייל שלך",
+    pattern: {
+      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+      message: '!אנא להזין דוא"ל תקף'
+    }
+  };
+  const passwordValidate = {
+    required: "אנא הזן סיסמה ",
+    minLength: {
+      value: 8,
+      message: "!סיסמה חייבת להיות באורך 8 תווים לפחות"
+    },
+    maxLength: {
+      value: 20,
+      message: "!סיסמה חייבת להיות באורך של 20 תווים לכל היותר"
+    }
+  }
   const [open, setOpen] = useState(false);
   const [openRegister, setOpenRegister] = useState(false);
   const handleClickOpen = () => {
@@ -32,10 +49,10 @@ export default function Login() {
   const handleClose = () => {
     setOpen(false);
   }
-const handleRegister=()=>{
-  handleClose();
-  setOpenRegister(true);
-}
+  const handleRegister = () => {
+    handleClose();
+    setOpenRegister(true);
+  }
   const generatePasswordHash = (password) => {
     const hashedPassword = CryptoJS.SHA256(password).toString();
     return hashedPassword;
@@ -55,10 +72,10 @@ const handleRegister=()=>{
 
   return (
     <React.Fragment>
-       <Button variant="outlined" color="inherit" onClick={handleClickOpen}>
+      <Button variant="outlined" color="inherit" onClick={handleClickOpen}>
         התחבר
       </Button>
-      <Dialog
+      <Dialog dir='rtl'
         open={open}
         onClose={handleClose}
         PaperProps={{
@@ -69,34 +86,27 @@ const handleRegister=()=>{
         <DialogTitle>התחבר</DialogTitle>
         <DialogContent>
           <DialogContentText> ברוך שובך! אנא היכנס כדי להמשיך</DialogContentText>
-          <TextField autoFocus margin="normal"  name="email"
-            label="Email Address" type="email" variant="standard"
-            {...register("email", {
-              required: "Please Enter Your Email!",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: "Please Enter A Valid Email!"
-              }
-            })} />
+
+          <TextField autoFocus margin="normal" name="email"
+            label="כתובת אימייל" type="email" variant="standard"
+            {...register("email", emailValidate)} />
+
           <InputLabel>{errors.email?.message}</InputLabel>
+
           <TextField autoFocus margin="normal" name="password"
-            label="Password" type="password" variant="standard"
-            {...register("password", {
-              required: "Please Enter Your Password",
-              minLength: {
-                value: 8,
-                message: "Password must be at least 8 characters long!"
-              }
-            })} />
+            label="סיסמה" type="password" variant="standard"
+            {...register("password", passwordValidate)} />
+
           <InputLabel>{errors.password?.message}</InputLabel>
-          <DialogContentText> <Link onClick={handleRegister}>הירשם</Link>?אינך רשום עדיין</DialogContentText>
+
+          <DialogContentText>אינך רשום עדיין ?<Link onClick={handleRegister}>הירשם</Link></DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>בטל</Button>
           <Button type="submit">שמור</Button>
         </DialogActions>
       </Dialog>
-      {openRegister&&<UserRegistrationForm/>}
+      {openRegister && <UserRegistrationForm emailValidate={emailValidate} passwordValidate={passwordValidate}/>}
     </React.Fragment>
   );
 }
--- client/src/components/UserRegistrationForm.jsx
+++ client/src/components/UserRegistrationForm.jsx
@@ -15,7 +15,7 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 
-export default function UserRegistrationForm() {
+export default function UserRegistrationForm(props) {
     const APIrequest = new APIrequests()
     const { register, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
@@ -24,7 +24,22 @@ export default function UserRegistrationForm() {
             password: '',
         }
     })
-
+    const { emailValidate, passwordValidate } = props;
+    const userNameValidate = {
+        required: "אנא הזן שם משתמש",
+        pattern: {
+            value: /^[a-z\u0590-\u05fe]+$/i,
+            message: "נא להזין רק תווים אלפביתיים"
+        },
+        minLength: {
+            value: 2,
+            message: "!שם המשתמש חייב להיות באורך 2 תווים לפחות"
+        },
+        maxLength: {
+            value: 20,
+            message: "!שם המשתמש חייב להיות באורך של 20 תווים לכל היותר"
+        }
+    }
     const [open, setOpen] = useState(true);
 
     const handleClose = () => {
@@ -39,9 +54,9 @@ export default function UserRegistrationForm() {
     const registerUser = async (userDetails) => {
         handleClose();
         const hash_password = generatePasswordHash(userDetails.password);
-        const response = await APIrequest.postRequest(`/authentication/login`, { "email": userDetails.email, "password": hash_password });
+        const response = await APIrequest.postRequest(`/authentication/register`, { "userName":userDetails.userName,"email": userDetails.email, "password": hash_password });
         if (!response.ok)
-            throw new Error('error, please try egain whith anouther email or password or sign up')
+           alert('error, please try egain whith anouther email or password or sign up')
         const data = response.json();
     }
 
@@ -62,46 +77,18 @@ export default function UserRegistrationForm() {
                 <DialogContent>
                     <DialogContentText> ברוך הבא! אנא היכנס כדי להמשיך</DialogContentText>
                     <TextField autoFocus margin="normal" name="userName"
-                        label="user name" type="text" variant="standard"
-                        {...register("userName", {
-                            required: "Please Enter Your user name",
-                            pattern: {
-                                value: /^[a-zA-Z]+$/,
-                                message: "Please Enter only alphabetic characters"
-                            },
-                            minLength: {
-                                value: 2,
-                                message: "user name must be at least 2 characters long!"
-                            },
-                            maxLength:{
-                                value: 20,
-                                message: "user name must be maximum 20 characters long!"
-                            }
-                        })} />
-                         <InputLabel>{errors.userName?.message}</InputLabel>
+                        label="שם משתמש" type="text" variant="standard"
+                        {...register("userName",userNameValidate)} />
+                    <InputLabel>{errors.userName?.message}</InputLabel>
+
                     <TextField autoFocus margin="normal" name="email"
-                        label="Email Address" type="email" variant="standard"
-                        {...register("email", {
-                            required: "Please Enter Your Email!",
-                            pattern: {
-                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message: "Please Enter A Valid Email!"
-                            }
-                        })} />
+                        label="כתובת אימייל" type="email" variant="standard"
+                        {...register("email", emailValidate)} />
                     <InputLabel>{errors.email?.message}</InputLabel>
+
                     <TextField autoFocus margin="normal" name="password"
                         label="Password" type="password" variant="standard"
-                        {...register("password", {
-                            required: "Please Enter Your Password",
-                            minLength: {
-                                value: 8,
-                                message: "Password must be at least 8 characters long!"
-                            },
-                            maxLength:{
-                                value: 20,
-                                message: "Password must be maximum 20 characters long!"
-                            }
-                        })} />
+                        {...register("password", passwordValidate)} />
                     <InputLabel>{errors.password?.message}</InputLabel>
                 </DialogContent>
                 <DialogActions>
